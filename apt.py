64b32b9275b50e5be11dd32157814795ff750039
sh00t2kill <38101689+sh00t2kill@users.noreply.github.com>
2023-04-28 10:12:58 +1000

    Update apt.py

--- apt.py
+++ apt.py
@@ -1,23 +1,27 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
+from cleantext import clean
 import hassapi as hass
 
 class APT(hass.Hass):
 
     def initialize(self):
         self.items = self.args['items']
+        if "notify" in self.args:
+            self.notify_service = self.args['notify']
+        else:
+            self.notify_service = False
+
+        self.header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"}
 
         self.run_in(self.get_prices, 0)
         self.run_every(self.get_prices, datetime.datetime.now(), 4800)
-
-        self.header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"}
 
     def get_prices(self, kwargs):
         for chunk in self.items:
             url = chunk['url']
             name = chunk['name']
-            #threshold = chunk['below_threshold']
             self.log(f"Looking for {name} at {url}")
             page = requests.get(url,headers=self.header)
             if not page.ok: continue
@@ -39,4 +43,9 @@ class APT(hass.Hass):
                 below_threshold = "off"
                 if float(price) < float(threshold):
                     below_threshold = "on"
+                    binary_sensor_entity = self.get_entity("binary_" + entity + "_threshold")
+                    binary_sensor_state = binary_sensor_entity.get_state()
+                    if binary_sensor_state != below_threshold and self.notify_service:
+                        self.log(f"The threshold {threshold} has been met for {title}. The binary_sensor is currently set to {binary_sensor_state}. Send an alert!")
+                        self.notify(message=f"{title} has gone below the threshold!", name=self.notify_service)
                 self.set_state("binary_" + entity + "_threshold", state=below_threshold, friendly_name=title)
